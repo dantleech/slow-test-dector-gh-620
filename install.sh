b856dbd5ce0f6f4c63dd533991e20b213e3e2161
Daniel Leech <daniel@dantleech.com>
2024-10-25 12:09:08 +0100

    Use latest versions

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 #!/usr/bin/env bash
 
+SLOW_TEST_DETECTOR_VERSION=2.16.0
+PHPUNIT_VERSION=11.4.2
+
 set -e
+
+rm -Rf tools/phpunit
 mkdir -p tools/phpunit
 
 if [[ -z $(which curl) ]]; then
@@ -9,12 +14,12 @@ if [[ -z $(which curl) ]]; then
     exit 1
 fi
 
-curl --output phpunit --location https://phar.phpunit.de/phpunit-10.5.37.phar
+curl --output phpunit --location https://phar.phpunit.de/phpunit-${PHPUNIT_VERSION}.phar
 chmod +x phpunit
 mv phpunit tools/phpunit/phpunit
 
 # important: the file-extension (.phar) must be specified
-curl --output phpunit-slow-test-detector.phar --location https://github.com/ergebnis/phpunit-slow-test-detector/releases/download/2.15.1/phpunit-slow-test-detector.phar
+curl --output phpunit-slow-test-detector.phar --location https://github.com/ergebnis/phpunit-slow-test-detector/releases/download/${SLOW_TEST_DETECTOR_VERSION}/phpunit-slow-test-detector.phar
 chmod +x phpunit-slow-test-detector.phar
 mkdir -p tools/phpunit/extension/
 mv phpunit-slow-test-detector.phar tools/phpunit/extension/
